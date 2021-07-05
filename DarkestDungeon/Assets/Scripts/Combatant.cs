5ca2c7ea1fd94a1c5aca648c2714b4fd1bc2ba8d
Roprescu <Roprescualex@yahoo.com>
2021-07-05 17:47:22 +0300

    setup

---
+++ DarkestDungeon/Assets/Scripts/Combatant.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combatant : MonoBehaviour
+{
+    [SerializeField]
+    int m_Speed;
+    [SerializeField]
+    int m_HP;
+    [SerializeField]
+    int m_MaxHP;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
