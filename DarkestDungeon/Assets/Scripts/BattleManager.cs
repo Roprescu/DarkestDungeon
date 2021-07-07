cf1b92f599a4c3ad4339fe32c63e0e3be811ed07
Roprescu <Roprescualex@yahoo.com>
2021-07-07 14:18:34 +0300

    update

--- DarkestDungeon/Assets/Scripts/BattleManager.cs
+++ DarkestDungeon/Assets/Scripts/BattleManager.cs
@@ -2,14 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+class ReverseComparer : IComparer<int>
+{
+    public int Compare(int x, int y)
+    {
+        return -x.CompareTo(y);
+    }
+}
 
 public class BattleManager : MonoBehaviour
 {
     [SerializeField]
-    private List<Combatant> m_PartyList;
-    [SerializeField]
-    private List<Combatant> m_EnemyList;
+    List<Combatant> m_SceneCombatants;
+    SortedList<int, List<Combatant>> m_CombatantList;
+    Queue<Combatant> m_CombatabtQueue;
+
+    void Awake()
+    {
+        m_CombatantList = new SortedList<int, List<Combatant>>(new ReverseComparer());
+        foreach(Combatant combatant in m_SceneCombatants)
+        {
+            AddCombatant(combatant.GetStat(COMBATANT_STATS.Speed), combatant);
+        }
+
+        foreach(KeyValuePair<int, List<Combatant>> combatantPair in m_CombatantList)
+        {
+            foreach(Combatant combatant in combatantPair.Value)
+            {
+                Debug.Log(combatant.GetStat(COMBATANT_STATS.Speed) + " " + combatant.transform.name);
+            }
+        }
+    }
+
+    void AddCombatant(int _speed, Combatant _combatant)
+    {
+        if(!m_CombatantList.ContainsKey(_speed))
+        {
+            m_CombatantList[_speed] = new List<Combatant>();
+        }
+        m_CombatantList[_speed].Add(_combatant);
+    }
+
 
     // Start is called before the first frame update
     void Start()
--- DarkestDungeon/Assets/Scripts/Combatant.cs
+++ DarkestDungeon/Assets/Scripts/Combatant.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum COMBATANT_STATS
+{
+    Invalid,
+    Speed
+}
+
 public class Combatant : MonoBehaviour
 {
+    [SerializeField]
+    Transform m_YourTurnUI;
     [SerializeField]
     int m_Speed;
     [SerializeField]
@@ -13,12 +21,30 @@ public class Combatant : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    public virtual void TurnHandler()
+    {
         
+    }
+
+
+    public int GetStat(COMBATANT_STATS _stat)
+    {
+        switch(_stat)
+        {
+            case COMBATANT_STATS.Speed:
+                return m_Speed;
+            default:
+                Debug.Log("STAT INVALID");
+                return -999;
+        }
     }
 }
